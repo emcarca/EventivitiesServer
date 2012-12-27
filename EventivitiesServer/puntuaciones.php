1f5f6da52572ebc96e53d9a103467eba1344d6f4
rbrodyr <marcosgallach@gmail.com>
2012-12-27 12:59:42 +0100

    servicio obtenerpuntuacionesevento acabado y probado

--- EventivitiesServer/puntuaciones.php
+++ EventivitiesServer/puntuaciones.php
@@ -2,11 +2,11 @@
 include_once('globales.php');
 include_once('bdadministrador.php');
 
-class eventos{
+class puntuaciones{
 
 	public function getPuntuaciones(){
 		$sql = "SELECT * ";
-		$sql .= " FROM Puntuacion ";
+		$sql .= " FROM Puntuacion ";		
 		$db = new bdadministrador();
 		return $db->executeQuery($sql);
 	}
@@ -18,63 +18,43 @@ class eventos{
 		return $db->executeQuery($sql);
 	}
 	
-	public function getJSONPuntuaciones(){
-		$json = "";
+	public function formateaJSON($result)
+	{		
+	
 		$i = 0;
-		$result = $this->getPuntuaciones();
-			
+		$json = "";
 		$json .= " { \"exito\" : \"1\" , \"puntuaciones\" : [ ";
 		if($result!=null)
-		{
+		{			
 			while($obj = $result->fetch_object()){
 				if($i > 0)
 					$json .= ",";
 				//"yyyy-MM-dd HH:mm"
-				$json .= " { \"idPuntuacion\" : ".$obj->idPuntuacion.", \"puntuacion\": \"".$obj->Puntuacion."\", \"idEvento\": \""
-						.$obj->idEvento."\", \"idUsuario\": \"".$obj->idUsuario."\" ";
-	
-	
+				$json .= " { \"idPuntuacion\" : ".$obj->idPuntuacion.", \"puntuacion\": \"".$obj->Puntuacion."\", \"idUsuario\": \""
+						.$obj->idUsuario."\", \"idEvento\": \"".$obj->idEvento."\" ";
+		
+		
 				$json .= "} ";
 				$i++;
 			}
-	
-			$json .= " ] ";
-			$json .= " } ";
-			return $json;
-	
-		}
-	}
-
-	public function getJSONEventosLocal($idLocal){
-		$json = "";
-		$i = 0;
-		$result = $this->getEventosLocal($idLocal);
 		
-		//$json .= " { \"eventos\" : [ ";
-		$json .= " { \"exito\" : \"1\" , \"eventos\" : [ ";
-		if($result!=null)
-		{
-			while($obj = $result->fetch_object()){
-				if($i > 0)
-					$json .= ",";
-					//"yyyy-MM-dd HH:mm"
- 				$json .= " { \"idEvento\" : ".$obj->idEvento.", \"nombre\": \"".$obj->Nombre."\", \"descripcion\": \""
- 						.$obj->Descripcion."\", \"fechaInicio\": \"".$obj->FechaInicio. "\", \"fechaFin\": \"".$obj->FechaFin."\",
-								 \"idLocal\": \"".$obj->idLocal."\" ";
-				
-
- 				$json .= "} "; 						
-				$i++;
-			}
-	
 			$json .= " ] ";
 			$json .= " } ";
 			return $json;
-
 		}
 	}
 	
-	
+	public function getJSONPuntuaciones(){	
+		$result = $this->getPuntuaciones();
+		$json=$this->formateaJSON($result);
+		return $json;		
+	}
+
+	public function getJSONPuntuacionesEvento($idEvento){
+		$result = $this->getPuntuacionesEvento($idEvento);
+		$json=$this->formateaJSON($result);
+		return $json;	
+	}	
 	
 	public function getJSONError($tag, $errormsg){
 		$response = array("tag" => $tag, "exito" => 0, "error" => 1, "error_msg" => $errormsg);
--- EventivitiesServer/service.obtenerpuntuacionesevento.php
+++ EventivitiesServer/service.obtenerpuntuacionesevento.php
@@ -1,9 +1,10 @@
 <?php
 include_once('puntuaciones.php');
 $puntuaciones = new puntuaciones();
-if(isset($_POST['idEvento'])){
-	echo $eventos->getJSONPuntuacionesEvento($_POST['idEvento']);
+echo $puntuaciones->getJSONPuntuaciones();
+/*if(isset($_POST['idEvento'])){
+	echo $puntuaciones->getJSONPuntuacionesEvento($_POST['idEvento']);
 } else {
-	echo $eventos->getJSONError('envio','Error en el envío de datos');
-}
+	echo $puntuaciones->getJSONError('envio','Error en el envío de datos');
+}*/
 ?>
